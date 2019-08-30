3c00f2b4a1bab031de25961f01b53d156a2b97b4
Piotr Binkowski <pbinkowski@antmicro.com>
2019-08-30 10:50:25 +0200

    tools: don't build tools by default
    Signed-off-by: Piotr Binkowski <pbinkowski@antmicro.com>

--- tools/runners.mk
+++ tools/runners.mk
@@ -7,17 +7,23 @@ RDIR := tools
 runners:
 
 # odin
-$(INSTALL_DIR)/bin/odin_II:
+odin: $(INSTALL_DIR)/bin/odin_II
+
+$(INSTALL_DIR)/bin/odin_II: init
 	@$(MAKE) -C $(RDIR)/odin_ii/ODIN_II/ build
 	@cp $(RDIR)/odin_ii/ODIN_II/odin_II $(INSTALL_DIR)/bin/
 
 # yosys
-$(INSTALL_DIR)/bin/yosys:
+yosys: $(install_dir)/bin/yosys
+
+$(install_dir)/bin/yosys: init
 	@$(MAKE) -C $(RDIR)/yosys ENABLE_TCL=0 ENABLE_ABC=0 ENABLE_GLOB=0 ENABLE_PLUGINS=0 ENABLE_READLINE=0 ENABLE_COVER=0
 	@cp $(RDIR)/yosys/yosys $(INSTALL_DIR)/bin/
 
 # icarus
-$(INSTALL_DIR)/bin/iverilog:
+icarus: $(INSTALL_DIR)/bin/iverilog
+
+$(INSTALL_DIR)/bin/iverilog: init
 	@cd $(RDIR)/icarus && autoconf
 	@cd $(RDIR)/icarus && ./configure --prefix=$(abspath $(INSTALL_DIR))/
 	@$(MAKE) -C $(RDIR)/icarus
@@ -25,14 +31,18 @@ $(INSTALL_DIR)/bin/iverilog:
 	@$(MAKE) -C $(RDIR)/icarus install
 
 # verilator
-$(INSTALL_DIR)/bin/verilator:
+verilator: $(INSTALL_DIR)/bin/verilator
+
+$(INSTALL_DIR)/bin/verilator: init
 	@cd $(RDIR)/verilator && autoconf
 	@cd $(RDIR)/verilator && ./configure --prefix=$(abspath $(INSTALL_DIR))/
 	@$(MAKE) -C $(RDIR)/verilator
 	@$(MAKE) -C $(RDIR)/verilator install
 
 # slang
-$(INSTALL_DIR)/bin/driver:
+slang: $(INSTALL_DIR)/bin/driver
+
+$(INSTALL_DIR)/bin/driver: init
 	@mkdir -p $(RDIR)/slang/build
 	@cd $(RDIR)/slang/build && cmake .. -DSLANG_INCLUDE_TESTS=OFF && make -j13
 	@cp $(RDIR)/slang/build/bin/* $(INSTALL_DIR)/bin/
