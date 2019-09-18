8765b82522e0807c53243fb6f7156095f1edae48
Piotr Binkowski <pbinkowski@antmicro.com>
2019-09-18 13:46:30 +0200

    runners: add proper python docstring
    Signed-off-by: Piotr Binkowski <pbinkowski@antmicro.com>

--- tools/BaseRunner.py
+++ tools/BaseRunner.py
@@ -1,30 +1,32 @@
 import subprocess
 
-##
-# This is the common base class shared by all runners.
-#
-# Each runner must either implement prepare_run_cb
-# or override the run method.
-#
-# prepare_run_cb is responsible for generating command to run
-# and preparing the command working directory if required by the tool.
-#
-# Runners must be located in tools/runners subdirectory
-# to be detected and launched by the Makefile.
-##
-
 
 class BaseRunner:
+    """Common base class shared by all runners
+    Each runner must either implement prepare_run_cb
+    or override the run method.
+
+    prepare_run_cb is responsible for generating command to run
+    and preparing the command working directory if required by the tool.
+
+    Runners must be located in tools/runners subdirectory
+    to be detected and launched by the Makefile.
+    """
+
     def __init__(self, name):
         self.name = name
 
-##
-# This method is called by the main runner script (tools/runner).
-# @param tmp_dir is a temporary directory created for this test run.
-# @param params is a dictionary with all metadata from the test file.
-#     All keys are stripped of their colons, ie. :tags: becomes tags.
-# @return A tuple containing command execution log and return code.
     def run(self, tmp_dir, params):
+        """Run the provided test case
+        This method is called by the main runner script (tools/runner).
+
+        Arguments:
+        tmp_dir -- temporary directory created for this test run.
+        params -- dictionary with all metadata from the test file.
+                  All keys are used without colons, ie. :tags: becomes tags.
+
+        Returns a tuple containing command execution log and return code.
+        """
         self.prepare_run_cb(tmp_dir, params)
 
         proc = subprocess.Popen(self.cmd, cwd=tmp_dir,
