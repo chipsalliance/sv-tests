9ebc6971ad3b9e924dd3d21098e5ce0d44fc9857
Alain Dargelas <alainmarcel@yahoo.com>
2019-09-30 20:51:18 -0700

    Malformed macro definitions
    Signed-off-by: Alain Dargelas <alainmarcel@yahoo.com>

--- tests/generic/preproc/preproc_test_10.sv
+++ tests/generic/preproc/preproc_test_10.sv
@@ -4,6 +4,6 @@
 :should_fail: 0
 :tags: 5.6.4
 */
-`define LONG_MACRO(
-    a,
+`define LONG_MACRO(\
+    a,\
     b, c) text goes here
--- tests/generic/preproc/preproc_test_9.sv
+++ tests/generic/preproc/preproc_test_9.sv
@@ -4,5 +4,5 @@
 :should_fail: 0
 :tags: 5.6.4
 */
-`define LONG_MACRO(
+`define LONG_MACRO(\
     a, b, c)
