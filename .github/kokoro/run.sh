b872dd81f2864e26446b302b937090e95449c349
Tim 'mithro' Ansell <me@mith.ro>
2019-11-12 10:58:07 -0800

    kokoro: Actually try and run the test suite.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- .github/kokoro/run.sh
+++ .github/kokoro/run.sh
@@ -10,10 +10,30 @@ source ./.github/kokoro/steps/git.sh
 
 echo
 echo "========================================"
+echo "Installing dependencies"
+echo "----------------------------------------"
+(
+	wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+	bash miniconda.sh -b -p $HOME/miniconda
+	source "$HOME/miniconda/etc/profile.d/conda.sh"
+	hash -r
+	conda config --set always_yes yes --set changeps1 no
+	conda install -q setuptools
+	conda update -q conda
+	conda info -a
+	conda env create --file conf/environment.yml
+	conda activate sv-test-env
+	pip install -r conf/requirements.txt
+)
+echo "----------------------------------------"
+
+echo
+echo "========================================"
 echo "Running tests"
 echo "----------------------------------------"
 (
-	true
+        make generate-tests
+        make report USE_ALL_RUNNERS=1
 )
 echo "----------------------------------------"
 
@@ -22,6 +42,7 @@ echo "========================================"
 echo "Copying tests logs"
 echo "----------------------------------------"
 (
+        touch out/report/.nojekyll
 	true
 )
 echo "----------------------------------------"
--- .github/kokoro/steps/hostsetup.sh
+++ .github/kokoro/steps/hostsetup.sh
@@ -25,39 +25,10 @@ sudo apt-get install -y \
         bison \
         build-essential \
         ca-certificates \
-        clang-format \
         cmake \
         curl \
         flex \
-        fontconfig \
         git \
-        jq \
-        nodejs \
-        psmisc \
-        python \
-        python3 \
-        python3-dev \
-        python3-virtualenv \
-        python3-yaml \
-        virtualenv \
+	wget \
 
 echo "----------------------------------------"
-
-(
-	cd /tmp
-	# Upgrade pstree to support the -T flag.
-	wget https://storage.googleapis.com/prjxray-deps-debs/psmisc_23.2-1_amd64.deb
-	sudo dpkg --install psmisc_23.2-1_amd64.deb
-	which pstree
-	pstree --help || true
-)
-
-echo "----------------------------------------"
-
-echo
-echo "========================================"
-echo "Getting diff2html to produce pretty database diffs"
-echo "----------------------------------------"
-(
-	sudo npm install -g diff2html-cli
-)
