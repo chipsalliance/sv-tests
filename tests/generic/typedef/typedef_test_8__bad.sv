8aa7669e2b4a26f328125931b48eaf812b46bb91
Wilson Snyder <wsnyder@wsnyder.org>
2019-11-16 11:24:19 -0500

    Fix tests; must predeclare types.
    Signed-off-by: Wilson Snyder <wsnyder@wsnyder.org>

--- tests/generic/typedef/typedef_test_8__bad.sv
+++ tests/generic/typedef/typedef_test_8__bad.sv
@@ -1,8 +1,8 @@
 /*
 :name: typedef_test_8
 :description: Test
-:should_fail: 0
+:should_fail: 1
 :tags: 6.18
 */
-typedef bit some_other_type;
+// some_other_type is not defined
 typedef some_other_type myalias;
