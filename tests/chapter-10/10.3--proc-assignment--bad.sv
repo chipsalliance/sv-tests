bf5122a6f10a93a05b7883f17fccd53bd876aa92
Wilson Snyder <wsnyder@wsnyder.org>
2019-11-16 10:51:26 -0500

    Fix 10.3.3 cont assignments. IEEE Table 10-1 says you cannot do procedural assignments to wire.
    Signed-off-by: Wilson Snyder <wsnyder@wsnyder.org>

--- tests/chapter-10/10.3--proc-assignment--bad.sv
+++ tests/chapter-10/10.3--proc-assignment--bad.sv
@@ -1,13 +1,15 @@
 /*
 :name: cont_assignment_delay
 :description: continuous assignment with delay test
-:should_fail: 0
-:tags: 10.3.3
+:should_fail: 1
+:tags: 10.3
 */
 module top(input a, input b);
 
 wire w;
 
-assign #10 w = a & b;
+// Illegal to procedurally assign to wire, IEEE Table 10-1
+initial
+	w = #10 a & b;
 
 endmodule
