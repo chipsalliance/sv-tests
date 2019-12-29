b972d5aa722dd712b05ce054adbdd929cfa0d391
MikePopoloski <mike@popoloski.com>
2019-12-29 18:44:54 -0500

    slang runner: Run on all modes, set correct flag for preprocessing mode
    Signed-off-by: MikePopoloski <mike@popoloski.com>

--- tools/runners/Slang.py
+++ tools/runners/Slang.py
@@ -3,12 +3,16 @@ from BaseRunner import BaseRunner
 
 class Slang(BaseRunner):
     def __init__(self):
-        super().__init__("slang", "slang-driver", {"preprocessing", "parsing"})
+        super().__init__("slang", "slang-driver")
 
         self.url = "https://github.com/MikePopoloski/slang"
 
     def prepare_run_cb(self, tmp_dir, params):
+        mode = params['mode']
+
         self.cmd = [self.executable]
+        if mode == 'preprocessing':
+            self.cmd += ['-E']
 
         for incdir in params['incdirs']:
             self.cmd.append('-I' + incdir)
