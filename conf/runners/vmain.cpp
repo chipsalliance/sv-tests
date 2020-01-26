5d96c82313186210818c3f4ff32a17920c9f572a
Wilson Snyder <wsnyder@wsnyder.org>
2020-01-26 16:54:43 -0500

    Fix Verilator driver to not false-fail when $time used
    Signed-off-by: Wilson Snyder <wsnyder@wsnyder.org>

--- conf/runners/vmain.cpp
+++ conf/runners/vmain.cpp
@@ -5,13 +5,17 @@
 #include <verilated.h>
 #include <Vtop.h>
 
+vluint64_t main_time = 0;
+double sc_time_stamp() { return main_time; }
+
 int main(int argc, char *argv[]) {
 	Verilated::commandArgs(argc, argv);
 
 	Vtop *top = new Vtop;
 
-	for (int i = 0; i < 1000 && !Verilated::gotFinish(); i++)
+	for (; main_time < 1000 && !Verilated::gotFinish(); ++main_time) {
 		top->eval();
+	}
 
 	top->final();
 
