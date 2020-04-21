4ef2944cacf692558d358378922249b94cba4100
Tomasz Gorochowik <tgorochowik@antmicro.com>
2020-04-21 14:34:22 +0200

    Add license header to top of all code files
    Fixes #749
    
    Signed-off-by: Tomasz Gorochowik <tgorochowik@antmicro.com>

--- tools/runners/Yosys.py
+++ tools/runners/Yosys.py
@@ -1,3 +1,9 @@
+# Copyright (C) 2020 The SymbiFlow Authors.
+#
+# Use of this source code is governed by a ISC-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/ISC
+
 import os
 
 from BaseRunner import BaseRunner
