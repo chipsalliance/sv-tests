545bb4df27360ed9f10e2aaafd544a038486d52f
Tomasz Gorochowik <tgorochowik@antmicro.com>
2020-04-22 11:10:58 +0200

    Add SPDX license identifiers
    Signed-off-by: Tomasz Gorochowik <tgorochowik@antmicro.com>

--- tools/BaseRunner.py
+++ tools/BaseRunner.py
@@ -3,6 +3,8 @@
 # Use of this source code is governed by a ISC-style
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/ISC
+#
+# SPDX-License-Identifier: ISC
 
 import psutil
 import resource
