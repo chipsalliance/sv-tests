c0f25c13f02d7d6c0a4489e5a6528ffb7af19c0c
Wilson Snyder <wsnyder@wsnyder.org>
2020-05-12 07:25:15 -0400

    chapter-7/queues/pop_back_assing.sv: Review feedback
    Signed-off-by: Wilson Snyder <wsnyder@wsnyder.org>

--- tests/chapter-7/queues/pop_back_assing.sv
+++ tests/chapter-7/queues/pop_back_assing.sv
@@ -11,10 +11,10 @@ int r;
 
 initial begin
 	q = { 2, 3, 4 };
-	r = q[$-1];
+	r = q[$];
 	q = q[0:$-1]; // void'(q.pop_back()) or q.delete(q.size-1)
 	$display(":assert: (%d == 2)", q.size);
-	$display(":assert: (%d == 3)", r);
+	$display(":assert: (%d == 4)", r);
 end
 
 endmodule
