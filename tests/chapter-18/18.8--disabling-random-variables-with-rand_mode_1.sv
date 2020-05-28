9b7be5fc8f74e36f0b70a46847cc8288af7d93cb
Wojciech Tatarski <wtatarski@antmicro.com>
2020-05-28 14:15:02 +0200

    chapter 18.8
    Signed-off-by: Wojciech Tatarski <wtatarski@antmicro.com>

---
+++ tests/chapter-18/18.8--disabling-random-variables-with-rand_mode_1.sv
@@ -0,0 +1,50 @@
+/*
+:name: disabling-random-variables-with-rand_mode_1
+:description: rand_mode() test
+:tags: 18.8 uvm
+*/
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+class a;
+    randc bit [7:0] x;
+    constraint c {x > 0; x < 12; }
+endclass
+
+class env extends uvm_env;
+
+  a obj = new;
+  int ret1, ret2;
+
+  function new(string name, uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+  
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    begin
+      obj.x.rand_mode(0);
+      ret1 = obj.x.rand_mode();
+      ret2 = obj.randomize();
+      if(ret1 == 0 && ret2 == 0) begin
+        `uvm_info("RESULT", $sformatf("ret1 = %0d ret2 = %0d SUCCESS", ret1, ret2), UVM_LOW);
+      end else begin
+        `uvm_info("RESULT", $sformatf("ret1 = %0d ret2 = %0d FAILED", ret1, ret2), UVM_LOW);
+      end
+    end
+    phase.drop_objection(this);
+  endtask: run_phase
+  
+endclass
+
+module top;
+
+  env environment;
+
+  initial begin
+    environment = new("env");
+    run_test();
+  end
+  
+endmodule
---
+++ tests/chapter-18/18.8--disabling-random-variables-with-rand_mode_5.sv
@@ -0,0 +1,52 @@
+/*
+:name: disabling-random-variables-with-rand_mode_5
+:description: rand_mode() test
+:should_fail_because: The rand_mode() method is built-in and cannot be overridden.
+:tags: 18.8 uvm
+*/
+
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+class a;
+    rand int x;
+    function int rand_mode();
+        return 1;
+    endfunction
+endclass
+
+class env extends uvm_env;
+
+  a obj = new;
+  int ret;
+
+  function new(string name, uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+  
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    begin
+      ret = obj.x.rand_mode();
+
+      if(ret == 1) begin
+        `uvm_info("RESULT", $sformatf("ret = %0d SUCCESS", ret), UVM_LOW);
+      end else begin
+        `uvm_info("RESULT", $sformatf("ret = %0d FAILED", ret), UVM_LOW);
+      end
+    end
+    phase.drop_objection(this);
+  endtask: run_phase
+  
+endclass
+
+module top;
+
+  env environment;
+
+  initial begin
+    environment = new("env");
+    run_test();
+  end
+  
+endmodule
