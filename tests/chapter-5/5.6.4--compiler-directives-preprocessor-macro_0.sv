98dd1f1224b9913d8614037c72d48412bb517e36
Wojciech Tatarski <wtatarski@antmicro.com>
2020-06-18 15:11:51 +0200

    Parse preprocessors macros from fusesoc source file
    Signed-off-by: Wojciech Tatarski <wtatarski@antmicro.com>

---
+++ tests/chapter-5/5.6.4--compiler-directives-preprocessor-macro_0.sv
@@ -0,0 +1,11 @@
+/*
+:name: compiler_directives_preprocessor_macro_0
+:description: Read preprocessing macro from template (:defines: marker)
+:tags: 5.6.4
+:defines: TEST_VAR
+*/
+
+`ifdef TEST_VAR
+`else
+TEST_VAR parsed not correctly from template
+`endif
