8213b29ccab5284ab8699f757d45920fc11ab2af
Paulo Henrique Silva <ph.silva@gmail.com>
2020-07-21 14:34:25 -0300

    moore: add incdirs to moore_parse (#951)
    Signed-off-by: Paulo Henrique Silva <ph.silva@carta.com>

--- tools/runners/moore_parse.py
+++ tools/runners/moore_parse.py
@@ -7,4 +7,9 @@ class moore_parse(moore):
 
     def prepare_run_cb(self, tmp_dir, params):
         self.cmd = [self.executable, '--syntax']
+
+        for incdir in params['incdirs']:
+            self.cmd.append('-I')
+            self.cmd.append(incdir)
+
         self.cmd += params['files']
