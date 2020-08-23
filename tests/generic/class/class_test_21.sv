0ff5f39f3472f83e2a6b6c823c09e4b44f11dfd4
Wilson Snyder <wsnyder@wsnyder.org>
2020-08-23 12:42:05 -0400

    class_tests: Fix missing predeclarations, etc
    Signed-off-by: Wilson Snyder <wsnyder@wsnyder.org>

--- tests/generic/class/class_test_21.sv
+++ tests/generic/class/class_test_21.sv
@@ -3,4 +3,9 @@
 :description: Test
 :tags: 6.15 8.3
 */
+package Package;
+class Bar #(int X=0, int Y=1, int Z=2); endclass
+endpackage
+localparam x=3, y=4, z=5;
+
 class Foo extends Package::Bar #(x,y,z); endclass
