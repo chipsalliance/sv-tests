6c2c647204b925ed832c11bdfea492f130b4a55a
Tomasz Gorochowik <tgorochowik@antmicro.com>
2020-11-16 09:32:25 +0100

    Remove deadlock from `always` test
    Fixes #969
    
    Signed-off-by: Tomasz Gorochowik <tgorochowik@antmicro.com>

--- tests/chapter-9/9.2.2.1--always.sv
+++ tests/chapter-9/9.2.2.1--always.sv
@@ -1,11 +1,9 @@
 /*
 :name: always
 :description: always check
-:tags: 9.2.2.1
+:tags: 9.2.2.1 9.4.1
 */
 module always_tb ();
 	wire a = 0;
-	reg b = 0;
-	always
-		b = ~a;
+	always #5 a = ~a;
 endmodule
