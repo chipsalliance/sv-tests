7524074693a7a4094a591d27645a58471abf8ad8
Tomasz Gorochowik <tgorochowik@antmicro.com>
2020-12-03 16:05:25 +0100

    Add licenses to the new runners
    Signed-off-by: Tomasz Gorochowik <tgorochowik@antmicro.com>

--- tools/runners/UhdmVerilator.py
+++ tools/runners/UhdmVerilator.py
@@ -1,3 +1,14 @@
+#!/bin/false python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 The SymbiFlow Authors.
+#
+# Use of this source code is governed by a ISC-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/ISC
+#
+# SPDX-License-Identifier: ISC
+
 import os
 import shutil
 
