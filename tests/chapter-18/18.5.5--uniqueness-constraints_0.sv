06ee8c28538bf213f49b407309c1ed1385a53456
Naoya Hatta <dalance@gmail.com>
2020-12-25 15:36:13 +0900

    Fix syntax error of inside expression
    Signed-off-by: Naoya Hatta <dalance@gmail.com>

--- tests/chapter-18/18.5.5--uniqueness-constraints_0.sv
+++ tests/chapter-18/18.5.5--uniqueness-constraints_0.sv
@@ -6,6 +6,7 @@
 
 class a;
     rand int b1, b2;
-    constraint c1 { b1, b2 inside {3, 10}; }
-    constraint c2 { unique {b1, b2}; }
+    constraint c1 { b1 inside {3, 10}; }
+    constraint c2 { b2 inside {3, 10}; }
+    constraint c3 { unique {b1, b2}; }
 endclass
