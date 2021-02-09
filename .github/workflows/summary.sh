cf64c502237e0b7f2ae230697f2fa6340c70a611
Pawel Czarnecki <pczarnecki@antmicro.com>
2021-02-09 08:28:03 +0100

    Introduce automated summary PR comment
    Signed-off-by: Pawel Czarnecki <pczarnecki@antmicro.com>

---
+++ .github/workflows/summary.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+ANALYZER=$PWD"/tools/report_analyzer.py"
+OUT_DIR=$PWD"/out/report/"
+COMPARE_REPORT=$OUT_DIR"/tests_report.csv"
+BASE_REPORT=$OUT_DIR"/base_report.csv"
+CHANGES_SUMMARY_JSON=$OUT_DIR"/tests_summary.json"
+CHANGES_SUMMARY_MD=$OUT_DIR"/tests_summary.md"
+
+set -x
+set -e
+
+# Get a conda environment
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+source "$HOME/miniconda/etc/profile.d/conda.sh"
+hash -r
+conda config --set always_yes yes --set changeps1 no
+
+conda env create --file conf/environment.yml
+conda activate sv-test-env
+hash -r
+conda info -a
+
+# Get base report from sv-tests master run
+wget https://symbiflow.github.io/sv-tests-results/report.csv -O $BASE_REPORT
+
+# Delete headers from all report.csv
+for file in $(find ./out/report_* -name "*.csv" -print); do
+	sed -i.backup 1,1d $file
+done
+
+# concatenate test reports
+cat $(find ./out/report_* -name "*.csv" -print) >> $COMPARE_REPORT
+
+# Insert header at the first line of concatenated report
+sed -i 1i\ $(cat $(find ./out/report_* -name "*.csv.backup" -print | head -1) | head -1) $COMPARE_REPORT
+
+python $ANALYZER $COMPARE_REPORT $BASE_REPORT -o $CHANGES_SUMMARY_JSON -t $CHANGES_SUMMARY_MD
+
+set +e
+set +x
+
