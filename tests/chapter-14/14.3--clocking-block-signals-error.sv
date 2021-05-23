4f6fcec964995e4d5b22bebc83c27858d55e9e19
MikePopoloski <mike@popoloski.com>
2021-05-23 10:52:02 -0400

    Fix clocking block test, add a should_fail test to mirror
    Signed-off-by: MikePopoloski <mike@popoloski.com>

--- tests/chapter-14/14.3--clocking-block-signals-error.sv
+++ tests/chapter-14/14.3--clocking-block-signals-error.sv
@@ -10,9 +10,11 @@
 /*
 :name: clocking_block_signals
 :description: clocking block with signals test
+:should_fail_because: assigning to net from procedural context
+:type: simulation
 :tags: 14.3
 */
-module top(input clk, input a, output logic b, output logic c);
+module top(input clk, input a, output b, output c);
 
 clocking ck1 @(posedge clk);
 	default input #10ns output #5ns;
