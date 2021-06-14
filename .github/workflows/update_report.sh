1446b87f8227fdc726c3d24fc78337cd7b60b5b7
Tomasz Gorochowik <tgorochowik@antmicro.com>
2021-06-14 12:39:39 +0200

    Make the deploy script more verbose, fix pushing
    Signed-off-by: Tomasz Gorochowik <tgorochowik@antmicro.com>

--- .github/workflows/update_report.sh
+++ .github/workflows/update_report.sh
@@ -2,6 +2,8 @@
 
 export CURRENT_PATH=$PWD
 
+set -x
+
 git clone \
   git@github.com:SymbiFlow/sv-tests-results.git \
   --single-branch \
@@ -10,6 +12,11 @@ git clone \
   output
 
 cd output
+
+# XXX changing the branch here is only temporary, remove this line before merging
+git checkout -b gh-pages-gha-test
+# XXX
+
 rm -rf *
 cp -a $CURRENT_PATH/out/report/* -t .
 touch .nojekyll
@@ -21,12 +28,13 @@ Deploy $GIT_REF (build $GITHUB_RUN_ID)
 Build from $GITHUB_SHA
 EOF
 
-git commit \
-  -F $GIT_MESSAGE_FILE \
-  --author "SymbiFlow Robot <foss-fpga-tools@google.com>"
+git config user.name "SymbiFlow Robot"
+git config user.email "foss-fpga-tools@google.com"
+
+git commit -F $GIT_MESSAGE_FILE
 
 git show -s
 
-git push \
-  git@github.com:SymbiFlow/sv-tests-results.git \
-  gh-actions-tests
+# XXX change it to git push just before merging
+git push --set-upstream origin gh-pages-gha-test
+# XXX
