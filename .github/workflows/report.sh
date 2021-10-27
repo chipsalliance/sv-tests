3bc55b7e30e311220da97fac123874cfebf0e1c5
Tomasz Gorochowik <tgorochowik@antmicro.com>
2021-10-27 15:51:14 +0200

    Build before running tests

--- .github/workflows/report.sh
+++ .github/workflows/report.sh
@@ -3,13 +3,6 @@
 # Add local submodules
 git submodule update --init --recursive
 
-set -e
-source "$HOME/miniconda/etc/profile.d/conda.sh"
-hash -r
-conda activate sv-test-env
-hash -r
-set -x
-
 make $@ generate-tests
 cp -ar ./out/report_*/logs ./out/
 make $@ report
