acf1093ef61a956aa2a889447bb4ec7f06bad484
Sanchay <sanchaydubey0105@gmail.com>
2026-06-30 13:34:33 +0530

    tests: add basic unpacked multidimensional array compliance check
    Signed-off-by: Sanchay <sanchaydubey0105@gmail.com>

---
+++ tests/chapter-7/unpacked_multidim_array.sv
@@ -0,0 +1,13 @@
+/*
+:name: unpacked_multidim_array
+:description: Verifies that tools correctly parse and manipulate multi-dimensional unpacked arrays.
+:tags: 7.4
+*/
+module top;
+    int grid [2][3]; // 2 rows, 3 columns unpacked array
+
+    initial begin
+        grid[0][0] = 32'hA5A5A5A5;
+        grid[1][2] = 32'h5A5A5A5A;
+    end
+endmodule
